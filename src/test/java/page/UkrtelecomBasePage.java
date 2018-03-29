e5a182f82c146c04c2a67119a47b6770d565c49b
olapitskyi <olapitskiy@ukr.net>
2018-03-29 12:08:21 +0300

    new project

---
+++ src/test/java/page/UkrtelecomBasePage.java
@@ -0,0 +1,12 @@
+package page;
+
+import org.openqa.selenium.WebDriver;
+
+public class UkrtelecomBasePage {
+
+    WebDriver driver;
+
+    public UkrtelecomBasePage (WebDriver driver){
+        this.driver = driver;
+    }
+}
